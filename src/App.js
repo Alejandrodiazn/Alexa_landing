42c5c6816103cf99d5df8ff83f18c60ffdf460f7
DíazUx <idiaz16@alumnos.uaq.mx>
2022-04-28 15:40:07 -0500

    Avanzando a los componentes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 
+import ButtonImportant from './components/button-important';
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <ButtonImportant text={"Hola a todos"}></ButtonImportant>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload porfi.
         </p>
         <a
           className="App-link"
---
+++ src/components/button-important.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function ButtonImportant(props){
+    return(
+        <button>
+            <a>{props.text}</a>
+        </button>
+    );
+}
+
